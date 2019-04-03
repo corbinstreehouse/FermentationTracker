004132bd2ad575941d135247fb4bedae565edc70
corbin dunn <corbin@corbinstreehouse.com>
2019-04-02 20:57:36 -0700

    Initial Commit

---
+++ TiltTracker/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TiltTracker
+//
+//  Created by Corbin Dunn on 4/2/19.
+//  Copyright © 2019 Corbin Dunn. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
